68b2397a09daceb501e14ffd4d1ca00d99131c9c
Mayur <maysankoshti0031@gmail.com>
2026-05-18 20:36:28 +0530

    fixing core issues

--- src/app.js
+++ src/app.js
@@ -14,7 +14,8 @@ const app = express();
 
 app.use(
   cors({
-    origin: process.env.CLIENT_ORIGIN || "http://localhost:3000",
+    origin: process.env.CLIENT_ORIGIN,
+    methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true
   })
 );
